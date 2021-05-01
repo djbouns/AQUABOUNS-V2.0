9a04a8c19e3121ee5038a56bc1b1075119934865
djbouns <35084579+djbouns@users.noreply.github.com>
2021-05-01 11:48:08 +0200

    Delete AQUABOUNS_MEGA_V2.0 directory

--- AQUABOUNS_MEGA_V2.0/eclairage.h
+++
@@ -1,7 +0,0 @@
-#ifndef  _eclairage_h_
-#define _eclairage_h_
-
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Declarations des fonctions dans le .cpp $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-void eclairage();
-
-#endif
--- AQUABOUNS_MEGA_V2.0/oscillo.h
+++
@@ -1,7 +0,0 @@
-#ifndef  Oscillo_h_
-#define Oscillo_h_
-
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Declarations des fonctions dans le .cpp $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-void oscillateur();
-
-#endif
